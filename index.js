7b996cc36b4da264558e2d7a37c64046f6a4b733
Rey Abdul <reyabdul@gmail.com>
2022-09-12 12:51:17 -0400

    update index.js and api.js to properly connect api routes

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const express = require('express');
-const { default: mongoose, connect } = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/api');
-const DBPassword = "Gemini1987";
-const uri = `mongodb+srv://reyabdul:${DBPassword}@cluster0.q1o1sfb.mongodb.net/?retryWrites=true&w=majority`;
-require('dotenv').config();
+require('dotenv').config()
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
-
+const port = process.env.PORT || 6000;
 
 //Connect to the database
     //also ref: "https://www.makeuseof.com/mongodb-cluster-cloud-free-setup/"
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    client.connect(err => {
-      const collection = client.db("test").collection("devices");
-      // perform actions on the collection object
-      client.close();
-    });
+mongoose
+    .connect(process.env.DB, { useNewUrlParser: true })
+    .then(() => console.log(`Database connected successfully`))
+    .catch((err) => console.log(err));
 
 // Since mongoose's Promise is deprecated, we override it with Node's Promise
 mongoose.Promise = global.Promise;
@@ -36,15 +29,15 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use('api', routes)
 
-app.use((req, res, next) => {
+app.use('/api', routes)
+
+app.use((err, req, res, next) => {
   console.log(err);
   next();
 });
 
 //listening to port and see if it connects
 app.listen(port, () => {
-  connectMongo()  
   console.log(`Server running on port ${port}`);
 });
