92d09ccfae1a18b6652435583d6794d36d8796af
Rey Abdul <reyabdul@gmail.com>
2022-09-12 10:37:40 -0400

    create model in mongoose and update api routes with the new model

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  //next() -  returns an object with two properties done and value
+    //ref "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator/next"
   next();
 });
 
